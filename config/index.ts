90325f1628d0c72360a38d366b59d1f4a8073d0a
rishad <rishad.yamnoor@gmail.com>
2024-08-05 10:41:26 +0530

    wallet connect - context and provider

---
+++ config/index.ts
@@ -0,0 +1,28 @@
+
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+// Your WalletConnect Cloud project ID
+export const projectId = 'd25d30fb15568c9a05abe406f8883973'
+
+// Create a metadata object
+const metadata = {
+  name: 'QuestChain',
+  description: 'AppKit Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+})
