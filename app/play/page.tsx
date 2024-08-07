1aa827a6ea29339be40c0a456d201b2ff7db88fc
rishad <rishad.yamnoor@gmail.com>
2024-08-07 15:17:50 +0530

    implemented age bar for participation

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,13 +1,46 @@
-export default function Play() {
-  
+'use client'
 
+import React, { useEffect, useState } from 'react';
+import { deserialize } from '@anon-aadhaar/core';
+import { useRouter } from "next/navigation";
+import { useAnonAadhaar } from '@anon-aadhaar/react';
+import { useAccount } from "wagmi";
+
+
+export default function Play() {
+  const [anonAadhaar] = useAnonAadhaar();
+  const { isConnected } = useAccount();
+  const [eligibleUser, setEligibleUser] = useState(false)
+  const router = useRouter();
+
+
+  useEffect(() => {
+    const aaObj = localStorage.getItem("anonAadhaar");
+    const anonAadhaarProofs = JSON.parse(aaObj!).anonAadhaarProofs;
+
+    deserialize(
+      anonAadhaarProofs[Object.keys(anonAadhaarProofs).length - 1].pcd
+    ).then((result) => {
+      console.log("anonAadresulthaar", result);
+      setEligibleUser(result.proof.ageAbove18 === "1" && true)
   
+    });
+  }, [anonAadhaar]);
+
+  if(!isConnected){
+    router.push("./");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Who is your Winning Swimmer?</h1>
  {/* check if its connected to wallet and anon data available for age then show play page */}
-      
-
+    {!eligibleUser ? (
+      <h1>User must be above 18 to place a bet!</h1>
+    )
+  : (
+    <h1>Please place your bet</h1>
+  )}
     </main>
-  )
-}
+  ) 
+} 
--- config/index.ts
+++ config/index.ts
@@ -4,8 +4,7 @@ import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { cookieStorage, createStorage } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
 
-// Your WalletConnect Cloud project ID
-export const projectId = "d25d30fb15568c9a05abe406f8883973"
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 const metadata = {
   name: 'Play a Bit',
